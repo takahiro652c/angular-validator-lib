cdba8744d51188431b0abc91c1c41cd4e2b1dd94
takahiro652c <takahiro652c@gmail.com>
2017-11-25 01:43:03 +0900

    #8 validatorEnableIfの使用感を上方修正、validatorCheck系を追加

--- src/app/validator-enable-if.component.ts
+++ src/app/validator-enable-if.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { valueEqualCheckFn, valueEqualToCheckFn } from './check-function';
-import { validatorsEnableIf, returnErrorValidator, validatorsEnableIfRef } from './validator-enable-if';
+import {
+  validatorsEnableIfRef, validatorCheck, validatorCheckRef, validatorCheckRefLazy, validatorsEnableIf, validatorsEnableIfRefLazy
+} from './validator-enable-if';
+import { AbstractControl } from '@angular/forms/src/model';
 
 
 @Component({
@@ -13,15 +16,45 @@ import { validatorsEnableIf, returnErrorValidator, validatorsEnableIfRef } from 
       .errors: {{ forms.errors | json }}<br>
       <hr>
       <div>
-        <h3>validatorsEnableIf(returnErrorValidator(), valueEqualCheckFn((string))('1'))</h3>
+        <h3>validatorCheck(valueEqualCheckFn((number))(1))</h3>
         .valid: {{ forms.controls.form1.valid | json }}<br>.errors: {{ forms.controls.form1.errors | json }}<br>
-        <input formControlName="form1">
+        <input formControlName="form1" type="number">
       </div>
       <hr>
       <div>
-        <h3> validatorsEnableIfRef([Valicators.requred, Validators.email], valueEqualToCheckFn())([this.form1]))</h3>
+        <h3>validatorCheckRef(valueEqualToCheckFn(), [this.form1])</h3>
         .valid: {{ forms.controls.form2.valid | json }}<br>.errors: {{ forms.controls.form2.errors | json }}<br>
-        <input formControlName="form2" type="email">
+        <input formControlName="form2" type="number">
+      </div>
+      <hr>
+      <div>
+        <h3>validatorCheckRefLazy(valueEqualToCheckFn())([this.form2])</h3>
+        .valid: {{ forms.controls.form3.valid | json }}<br>.errors: {{ forms.controls.form3.errors | json }}<br>
+        <input formControlName="form3" type="number">
+      </div>
+      <hr>
+      <div>
+        <h3>validatorsEnableIf(Validators.maxLength(3), this.checkFn4)</h3>
+        .valid: {{ forms.controls.form4.valid | json }}<br>.errors: {{ forms.controls.form4.errors | json }}<br>
+        <input formControlName="form4">
+      </div>
+      <hr>
+      <div>
+        <h3>
+          validatorsEnableIfRef(
+            Validators.maxLength(5),
+            this.checkFn5,
+            [this.form1, this.form2, this.form3, this.form4]
+          )
+        </h3>
+        .valid: {{ forms.controls.form5.valid | json }}<br>.errors: {{ forms.controls.form5.errors | json }}<br>
+        <input formControlName="form5">
+      </div>
+      <hr>
+      <div>
+        <h3>validatorsEnableIfRefLazy([Validators.required, Validators.email], valueEqualToCheckFn())([this.form5])</h3>
+        .valid: {{ forms.controls.form6.valid | json }}<br>.errors: {{ forms.controls.form6.errors | json }}<br>
+        <input formControlName="form6" type="email">
       </div>
       <hr>
     </form>
@@ -29,9 +62,41 @@ import { validatorsEnableIf, returnErrorValidator, validatorsEnableIfRef } from 
   styleUrls: []
 })
 export class ValidatorEnableIfComponent {
-  form1 = new FormControl(null, validatorsEnableIf(returnErrorValidator(), valueEqualCheckFn<string>('1')));
+  form1 = new FormControl(null, validatorCheck(valueEqualCheckFn<number>(1)));
+  form2 = new FormControl(null, validatorCheckRef(valueEqualToCheckFn(), [this.form1]));
+  validatorLazy3 = validatorCheckRefLazy(valueEqualToCheckFn());
+  form3 = new FormControl(null, this.validatorLazy3([this.form2]));
+  form4 = new FormControl(null, validatorsEnableIf(Validators.maxLength(3), this.checkFn4));
+  form5 = new FormControl(
+    null,
+    validatorsEnableIfRef(
+      Validators.maxLength(5),
+      this.checkFn5,
+      [this.form1, this.form2, this.form3, this.form4]
+    )
+  );
+  validatorLazy6 = validatorsEnableIfRefLazy([Validators.required, Validators.email], valueEqualToCheckFn());
+  form6 = new FormControl(null, this.validatorLazy6([this.form5]));
   forms = new FormGroup({
     form1: this.form1,
-    form2: new FormControl(null, validatorsEnableIfRef([Validators.required, Validators.email], valueEqualToCheckFn())([this.form1])),
+    form2: this.form2,
+    form3: this.form3,
+    form4: this.form4,
+    form5: this.form5,
+    form6: this.form6,
   });
+
+  checkFn4(c: AbstractControl, refs?: AbstractControl[]): boolean {
+    return c.value === 'hoge';
+  }
+
+  checkFn5(c: AbstractControl, refs?: AbstractControl[]): boolean {
+    for (let i = 0; i < refs.length; i++) {
+      const value = refs[i].value;
+      if (value == null || value === '') {
+        return false;
+      }
+    }
+    return true;
+  }
 }
--- src/app/validator-enable-if.ts
+++ src/app/validator-enable-if.ts
@@ -3,16 +3,30 @@ import { AbstractControl, ValidationErrors, ValidatorFn, Validators } from '@ang
 export type CheckFn = (c: AbstractControl, refs?: AbstractControl[]) => boolean;
 export type ValidatorFnRef = (refs: AbstractControl[]) => ValidatorFn;
 
-// checkFnがtrueの場合だけvalidatorsによるvalidateを行う
-// checkFnがfalseの場合はnullを返す
-export function validatorsEnableIf(validators: ValidatorFn | ValidatorFn[], checkFn: CheckFn): ValidatorFn {
-  return validatorsEnableIfRef(validators, checkFn)([]);
+export function validatorCheck(checkFn: CheckFn, error?: ValidationErrors): ValidatorFn {
+  const validationError: ValidationErrors = error || { validatorCheck: true };
+  return validatorsEnableIf(returnErrorValidator(validationError), checkFn);
 }
 
-// checkFnRefがtrueの場合だけvalidatorsによるvalidateを行う
-// checkFnRefがfalseの場合はnullを返す
-// ただし，validatorsEnableIfRef(validators, checkFnRef)(refs)のようにrefsを指定する
-export function validatorsEnableIfRef(validators: ValidatorFn | ValidatorFn[], checkFn: CheckFn): ValidatorFnRef {
+export function validatorCheckRef(checkFn: CheckFn, refs: AbstractControl[], error?: ValidationErrors): ValidatorFn {
+  const validationError: ValidationErrors = error || { validatorCheckRef: true };
+  return validatorsEnableIfRef(returnErrorValidator(validationError), checkFn, refs);
+}
+
+export function validatorCheckRefLazy(checkFn: CheckFn, error?: ValidationErrors): ValidatorFnRef {
+  const validationError: ValidationErrors = error || { validatorCheckRef: true };
+  return validatorsEnableIfRefLazy(returnErrorValidator(validationError), checkFn);
+}
+
+export function validatorsEnableIf(validators: ValidatorFn | ValidatorFn[], checkFn: CheckFn): ValidatorFn {
+  return validatorsEnableIfRef(validators, checkFn, []);
+}
+
+export function validatorsEnableIfRef(validators: ValidatorFn | ValidatorFn[], checkFn: CheckFn, refs: AbstractControl[]): ValidatorFn {
+  return validatorsEnableIfRefLazy(validators, checkFn)(refs);
+}
+
+export function validatorsEnableIfRefLazy(validators: ValidatorFn | ValidatorFn[], checkFn: CheckFn): ValidatorFnRef {
   const validator = Array.isArray(validators) ? Validators.compose(validators) : validators;
   let subscribe = false;
 
