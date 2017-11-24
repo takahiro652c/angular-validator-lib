9b4d20176bb194f75e3d8b6315c0fc4fbc3f481d
t-inoue <t-inoue@osaka-univ.coop>
2017-11-24 12:56:40 +0900

    #6 validatorsEnableIf追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GroupValidatorComponent } from './group-validator.component';
+import { ValidatorEnableIfComponent } from './validator-enable-if.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GroupValidatorComponent,
+    ValidatorEnableIfComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/check-function.ts
@@ -0,0 +1,29 @@
+import { AbstractControl } from '@angular/forms';
+import { CheckFn } from 'app/validator-enable-if';
+
+export function valueEqualCheckFn<T>(val: T): CheckFn {
+  return (c: AbstractControl, refs?: AbstractControl[]): boolean => {
+    return c.value === val;
+  }
+}
+
+export function valueEqualToCheckFn(): CheckFn {
+  return (c: AbstractControl, refs?: AbstractControl[]): boolean => {
+    if (!refs) {
+      return true;
+    } else {
+      for (let i = 0; i < refs.length; i++) {
+        if (c.value !== refs[i].value) {
+          return false;
+        }
+      }
+      return true;
+    }
+  }
+}
+
+export function valueInArrayCheckFn<T>(array: T[]): CheckFn {
+  return (c: AbstractControl, refs?: AbstractControl[]): boolean => {
+    return array.indexOf(c.value) < 0 ? false : true;
+  }
+}
